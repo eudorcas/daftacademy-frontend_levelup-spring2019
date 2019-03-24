0cb7f4b3ade7bfd448bb48d4650fb27586f0f0db
eudorcas <klaudia.butkiewicz09@gmail.com>
2019-03-24 21:55:45 +0100

    Praca domowa nr 1

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [[
+                            '@babel/preset-env', {
+                                targets: {
+                                    browsers: ['> 1%', 'last 2 versions']
+                                }
+                            }
+                        ]]
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin()]
+};
