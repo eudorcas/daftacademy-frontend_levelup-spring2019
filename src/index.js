0f9730726f2e87682c62a852af76de178c547df0
eudorcas <klaudia.butkiewicz09@gmail.com>
2019-04-01 23:15:02 +0200

    Webpack config

--- src/index.js
+++ src/index.js
@@ -1,3 +1,1 @@
-import func from './script';
 
-func();
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,13 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var path = require('path');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
-    entry: './src/index.js',
+    entry: [
+        './src/index.js'
+    ],
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
@@ -13,34 +18,86 @@ module.exports = {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: {
-                    loader: "babel-loader",
+                    loader: 'babel-loader',
                     options: {
-                        presets: [[
-                            '@babel/preset-env', {
-                                targets: {
-                                    browsers: ['> 1%', 'last 2 versions']
-                                }
-                            }
-                        ]]
+                        presets: ['@babel/preset-env']
                     }
                 }
             },
             {
                 test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
+                use: ['style-loader', 'css-loader']
             },
             {
-                test: /\.scss$/,
+                test: /\.s(a|c)ss$/,
                 use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
+                    isProduction
+                        ? MiniCssExtractPlugin.loader
+                        : { loader: 'style-loader', options: { sourceMap: true } },
+                    { loader: 'css-loader', options: { sourceMap: isProduction } },
+                    { loader: 'postcss-loader', options: { sourceMap: isProduction } },
+                    { loader: 'sass-loader', options: { sourceMap: isProduction } }
                 ]
-            }
+
+            },
+
         ]
     },
-    plugins: [new HtmlWebpackPlugin()]
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
+    ],
 };
+// var HtmlWebpackPlugin = require('html-webpack-plugin');
+// var path = require('path');
+//
+// module.exports = {
+//     entry: './src/index.js',
+//     output: {
+//         filename: 'main.js',
+//         path: path.resolve(__dirname, 'dist')
+//     },
+//     module: {
+//         rules: [
+//             {
+//                 test: /\.js$/,
+//                 exclude: /node_modules/,
+//                 use: {
+//                     loader: "babel-loader",
+//                     options: {
+//                         presets: [[
+//                             '@babel/preset-env', {
+//                                 targets: {
+//                                     browsers: ['> 1%', 'last 2 versions']
+//                                 }
+//                             }
+//                         ]]
+//                     }
+//                 }
+//             },
+//             {
+//                 test: /\.css$/,
+//                 use: [
+//                     'style-loader',
+//                     'css-loader'
+//                 ]
+//             },
+//             {
+//                 test: /\.scss$/,
+//                 use: [
+//                     'style-loader',
+//                     'css-loader',
+//                     'sass-loader'
+//                 ]
+//             }
+//         ]
+//     },
+//     plugins: [new HtmlWebpackPlugin({
+//         template: './src/index.html'
+//     })]
+// };
